887640e0e54cba8c3e496b88dc33ef19e3c2b7f1
Ibrahim <ibrahim0ineizeh@gmail.com>
2025-03-30 03:07:59 +0300

    Added textures and made the trees smaller

--- src/script.js
+++ src/script.js
@@ -16,36 +16,157 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+// Floor 
+const floorAlphaTexture = textureLoader.load("./floor/alpha.jpg")
+const floorColorTexture = textureLoader.load("./floorTwo/brown_mud_diff.jpg")
+const floorARMTexture = textureLoader.load("./floorTwo/brown_mud_arm.jpg")
+const floorDispTexture = textureLoader.load("./floorTwo/brown_mud_disp.jpg")
+const floorNormalTexture = textureLoader.load("./floorTwo/brown_mud_nor_gl.jpg")
+
+floorColorTexture.colorSpace = THREE.SRGBColorSpace
+
+floorColorTexture.repeat.set(8, 8)
+floorColorTexture.wrapS = THREE.RepeatWrapping
+floorColorTexture.wrapT = THREE.RepeatWrapping
+
+floorARMTexture.repeat.set(8, 8)
+floorARMTexture.wrapS = THREE.RepeatWrapping
+floorARMTexture.wrapT = THREE.RepeatWrapping
+
+floorDispTexture.repeat.set(8, 8)
+floorDispTexture.wrapS = THREE.RepeatWrapping
+floorDispTexture.wrapT = THREE.RepeatWrapping
+
+floorNormalTexture.repeat.set(8, 8)
+floorNormalTexture.wrapS = THREE.RepeatWrapping
+floorNormalTexture.wrapT = THREE.RepeatWrapping
+
+// Walls
+const wallColorTexture = textureLoader.load("./walls/seaworn_diff.jpg")
+const wallARMTexture = textureLoader.load("./walls/seaworn_arm.jpg")
+const wallNormalTexture = textureLoader.load("./walls/seaworn_nor_gl.jpg")
+
+wallColorTexture.colorSpace = THREE.SRGBColorSpace
+
+wallColorTexture.repeat.set(3, 1.75)
+wallColorTexture.wrapS = THREE.RepeatWrapping
+wallColorTexture.wrapT = THREE.RepeatWrapping
+
+wallARMTexture.repeat.set(3, 1.75)
+wallARMTexture.wrapS = THREE.RepeatWrapping
+wallARMTexture.wrapT = THREE.RepeatWrapping
+
+wallNormalTexture.repeat.set(3, 1.75)
+wallNormalTexture.wrapS = THREE.RepeatWrapping
+wallNormalTexture.wrapT = THREE.RepeatWrapping
+
+// Wood
+const woodColorTexture = textureLoader.load("./wood/bark_diff.jpg")
+const woodARMTexture = textureLoader.load("./wood/brak_arm.jpg")
+const woodNormalTexture = textureLoader.load("./wood/bark_nor_gl.jpg")
+
+woodColorTexture.colorSpace = THREE.SRGBColorSpace
+
+woodColorTexture.repeat.set(1, 2)
+woodColorTexture.wrapS = THREE.RepeatWrapping
+woodColorTexture.wrapT = THREE.RepeatWrapping
+
+woodARMTexture.repeat.set(1, 2)
+woodARMTexture.wrapS = THREE.RepeatWrapping
+woodARMTexture.wrapT = THREE.RepeatWrapping
+
+woodNormalTexture.repeat.set(1, 2)
+woodNormalTexture.wrapS = THREE.RepeatWrapping
+woodNormalTexture.wrapT = THREE.RepeatWrapping
+
+// Bush
+const bushColorTexture = textureLoader.load("./bush/bush_diff.jpg")
+const bushRoughTexture = textureLoader.load("./bush/bush_rough.jpg")
+const bushNormalTexture = textureLoader.load("./bush/bush_nor.jpg")
+const bushDispTexture = textureLoader.load("./bush/bush_disp.jpg")
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace
+
+bushColorTexture.repeat.set(1, 1)
+bushColorTexture.wrapS = THREE.RepeatWrapping
+bushColorTexture.wrapT = THREE.RepeatWrapping
+
+bushRoughTexture.repeat.set(1, 1)
+bushRoughTexture.wrapS = THREE.RepeatWrapping
+bushRoughTexture.wrapT = THREE.RepeatWrapping
+
+bushNormalTexture.repeat.set(1, 1)
+bushNormalTexture.wrapS = THREE.RepeatWrapping
+bushNormalTexture.wrapT = THREE.RepeatWrapping
+
+bushDispTexture.repeat.set(1, 1)
+bushDispTexture.wrapS = THREE.RepeatWrapping
+bushDispTexture.wrapT = THREE.RepeatWrapping
+
+// Door 
+const doorColorTexture = textureLoader.load("./door/woodenDoor_diff.jpg")
+const doorARMTexture = textureLoader.load("./door/woodenDoor_arm.jpg")
+const doorNormalTexture = textureLoader.load("./door/woodenDoor_normal_gl.jpg")
+const doorDispTexture = textureLoader.load("./door/woodenDoor_disp.jpg")
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace
+
+/**
  * House
  */
 
 // Floor 
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(40, 40),
-    new THREE.MeshStandardMaterial()
+    new THREE.PlaneGeometry(40, 40, 200, 200),
+    new THREE.MeshStandardMaterial({
+        alphaMap : floorAlphaTexture,
+        transparent: true,
+        map: floorColorTexture,
+        aoMap: floorARMTexture,
+        roughnessMap: floorARMTexture,
+        metalnessMap: floorARMTexture,
+        displacementMap: floorDispTexture,
+        normalMap: floorNormalTexture,
+        displacementScale: 0.5,
+        displacementBias: -0.08
+    })
 )
 floor.rotation.x= - Math.PI * 0.5
 scene.add(floor)
 
+// gui.add(floor.material, 'displacementScale').min(0).max(1).step(0.001).name("DisplacementScale")
+// gui.add(floor.material, 'displacementBias').min(-1).max(1).step(0.001).name("DisplacementBias")
+
 const castle = new THREE.Group()
 scene.add(castle)
 
+const wallMaterial = new THREE.MeshStandardMaterial({
+    map: wallColorTexture,
+    aoMap: wallARMTexture,
+    roughnessMap: wallARMTexture,
+    metalnessMap: wallARMTexture,
+    normalMap: wallNormalTexture,
+})
+
 // Walls
 const walls = new THREE.Mesh(
-    new THREE.BoxGeometry(7, 3.5, 7),
-    new THREE.MeshStandardMaterial({color: "gray"})
+    new THREE.BoxGeometry(7, 3.5, 7, 30, 30, 30),
+    wallMaterial
 )
 walls.position.y += 1.75
 castle.add(walls)
 
 //Towers 
 const towerGeometry = new THREE.CylinderGeometry(1, 1, 5, 32);
-const towerMaterial = new THREE.MeshStandardMaterial({color: "gray"});
 
 // Tower Top Right
 const towerTR = new THREE.Mesh(
     towerGeometry,
-    towerMaterial
+    wallMaterial
 );
 towerTR.position.x += 3.3;
 towerTR.position.y += 2.25; 
@@ -54,7 +175,7 @@ castle.add(towerTR)
 
 const towerTL = new THREE.Mesh(
     towerGeometry,
-    towerMaterial
+    wallMaterial
 );
 towerTL.position.x -= 3.3;
 towerTL.position.y += 2.25; 
@@ -63,7 +184,7 @@ castle.add(towerTL)
 
 const towerBR = new THREE.Mesh(
     towerGeometry,
-    towerMaterial
+    wallMaterial
 );
 towerBR.position.x += 3.3;
 towerBR.position.y += 2.25; 
@@ -72,7 +193,7 @@ castle.add(towerBR)
 
 const towerBL = new THREE.Mesh(
     towerGeometry,
-    towerMaterial
+    wallMaterial
 );
 towerBL.position.x -= 3.3;
 towerBL.position.y += 2.25; 
@@ -82,26 +203,47 @@ castle.add(towerBL)
 // Castle Door 
 const castleDoor = new THREE.Mesh(
     new THREE.PlaneGeometry(2.5, 2),
-    new THREE.MeshStandardMaterial({color: "brown"})
+    new THREE.MeshStandardMaterial({
+        map: doorColorTexture,
+        aoMap: doorARMTexture,
+        roughnessMap: doorARMTexture,
+        metalnessMap: doorARMTexture,
+        displacementMap: doorDispTexture,
+        normalMap: doorNormalTexture,
+        displacementScale: 0.3,
+        displacementBias: -0.13
+    })
 );
 castleDoor.position.y += 0.7;
 castleDoor.position.z += 3.51;
 castle.add(castleDoor)
 
 // Trees
-const treeBaseGeo = new THREE.CylinderGeometry(0.08, 0.15, 2, 8)
-const treeBranchOneGeo = new THREE.CylinderGeometry(0.03, 0.05, 1, 8)
-const treeBranchTwoGeo =  new THREE.CylinderGeometry(0.02, 0.05, 2, 8)
-const treeBushGeo = new THREE.SphereGeometry(1.1, 16, 8)
-const treeBranchMaterial = new THREE.MeshStandardMaterial({color : "brown"})
-const treeBushMaterial = new THREE.MeshStandardMaterial({color : "green"})
+const treeBaseGeo = new THREE.CylinderGeometry(0.08, 0.15, 1, 6)
+const treeBranchOneGeo = new THREE.CylinderGeometry(0.03, 0.05, 0.5, 8)
+const treeBranchTwoGeo =  new THREE.CylinderGeometry(0.02, 0.05, 1, 8)
+const treeBushGeo = new THREE.SphereGeometry(0.8, 32, 16)
+const treeBranchMaterial = new THREE.MeshStandardMaterial({
+    map: woodColorTexture,
+    aoMap: woodARMTexture,
+    roughnessMap: woodARMTexture,
+    metalnessMap: woodARMTexture,
+    normalMap: woodNormalTexture,
+})
+const treeBushMaterial = new THREE.MeshStandardMaterial({
+    map: bushColorTexture,
+    normalMap: bushNormalTexture,
+    roughnessMap: bushRoughTexture,
+    displacementMap: bushDispTexture,
+    displacementScale: 0.8
+})
 
 //The Trees Group 
 const trees = new THREE.Group()
 scene.add(trees)
 
 // Creating all Trees
-for (let i = 0; i < 40; i++){
+for (let i = 0; i < 80; i++){
     
     // Creating an angle for the tree 
     const angle = Math.random() * Math.PI * 2
@@ -120,8 +262,8 @@ for (let i = 0; i < 40; i++){
         treeBranchOneGeo,
         treeBranchMaterial
     )
-    treeBranchOne.position.y += 0.9
-    treeBranchOne.position.x -= 0.3
+    treeBranchOne.position.y += 0.35
+    treeBranchOne.position.x -= 0.1
     treeBranchOne.rotation.z += Math.PI * 0.2
     
     // Creating a tree branch
@@ -129,8 +271,8 @@ for (let i = 0; i < 40; i++){
        treeBranchTwoGeo,
        treeBranchMaterial
     )
-    treeBranchTwo.position.y += 1
-    treeBranchTwo.position.z += 0.5
+    treeBranchTwo.position.y += 0.4
+    treeBranchTwo.position.z += 0.2
     treeBranchTwo.rotation.z += Math.PI * 0.15
     treeBranchTwo.rotation.y += Math.PI * 0.5
     
@@ -139,7 +281,7 @@ for (let i = 0; i < 40; i++){
         treeBushGeo,
         treeBushMaterial
     )
-    treeBush.position.y += 2
+    treeBush.position.y += 1.3
     
     // Make a group for the tree components and add them to the Group
     const tree = new THREE.Group()
@@ -147,10 +289,11 @@ for (let i = 0; i < 40; i++){
     
     // Randomize trees
     tree.position.x = positionX
-    tree.position.y += 0.6
-    tree.position.y += Math.random() * 0.3
+    tree.position.y += 0.315
+    tree.position.y += Math.random() * 0.2
     tree.position.z = positionZ
-    tree.rotation.x = (Math.random() - 0.3) * 0.4 
+    tree.rotation.x = (Math.random() - 0.3) * 0.2
+    tree.rotation.y = Math.random() * Math.PI * 2;
     tree.rotation.z = (Math.random() - 0.3) * 0.3
     trees.add(tree)
 }
@@ -160,13 +303,17 @@ for (let i = 0; i < 40; i++){
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.7)
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.8)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1.2)
 directionalLight.position.set(3, 2, -8)
 scene.add(directionalLight)
+
+const directionalLight2 = new THREE.DirectionalLight('#ffffff', 0.3)
+directionalLight2.position.set(-5, 2, +8)
+scene.add(directionalLight2)
 
 /**
  * Sizes
